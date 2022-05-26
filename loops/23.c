1a6059f434c030203f37986752aa07ce9698ecbd
Gabriel-Lombardi <102254655+Gabriel-Lombardi@users.noreply.github.com>
2022-05-26 10:39:56 -0300

    Add files via upload

---
+++ loops/23.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char *argv[]) {
+  int n = atoi(argv[1]);
+
+  if (n < 2) printf("El número debe ser mayor o igual a 2\n");
+  else 
+  return 0;
+}
