70558e676987401795ade6c9d51d083011c4ea88
Gabriel-Lombardi <102254655+Gabriel-Lombardi@users.noreply.github.com>
2022-10-04 15:21:11 -0300

    Add files via upload

--- truco/truco.c
+++ truco/truco.c
@@ -7,7 +7,7 @@ int carta_mayor(int a, int b) {
 }
 
 int segunda_carta(int a, int b, int c) {
-  if ((a > b && a < c) || (a < b && a > c)) return a;
+  if ((a >= b && a <= c) || (a <= b && a >= c)) return a;
   else {
     return segunda_carta(b, c, a);
   }
@@ -17,15 +17,15 @@ int envido(int a, char p1, int b, char p2, int c, char p3) {
   int c_mayor = carta_mayor(carta_mayor(a,b), c);
   int c_media = segunda_carta(a, b, c);
 
-  if (a > 10)      a = 0;
-  else if (b > 10) b = 0;
-  else if (c > 10) c = 0;
+  if (a >= 10)      a = 0;
+  else if (b >= 10) b = 0;
+  else if (c >= 10) c = 0;
 
-  if (p1 =! p2 && p1 =! p3 && p2 =! p3) return c_mayor;
+  if (p1 != p2 && p1 != p3 && p2 != p3) return c_mayor;
+  else if (p1 == p2 && p2 == p3)        return 20 + c_mayor + c_media;
   else if (p1 == p2)                    return 20 + a + b;
   else if (p1 == p3)                    return 20 + a + c;
   else if (p2 == p3)                    return 20 + b + c;
-  else if (p1 == p2 && p2 == p3)        return 20 + c_mayor + c_media;
 }
 
 int main(int argc, char *argv[]) {
